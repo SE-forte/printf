c674de51a41d17ed11fbbfb4c5ef0765b594d6c2
cnagyei <cnagyei15@gmail.com>
2023-08-20 04:07:01 -0700

    printf

--- main.c
+++ main.c
@@ -23,6 +23,10 @@ int main(void)
     _printf("%s", "This sentence is retrieved from va_args!\n");
     printf("%s", "This sentence is retrieved from va_args!\n");
 
+    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+    printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+    printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
     _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
     printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
     _printf("%%");
